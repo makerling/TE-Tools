c9a1f43ef1577693efe5eba939d938da55799e8a
Stevan Vanderwerf <github@vanderling.com>
2021-10-26 22:43:07 -0500

    finished title annotations parsing

--- Actions/QA/pythonconvert.py
+++ Actions/QA/pythonconvert.py
@@ -1,4 +1,4 @@
-#import defusedxml.ElementTree as ET
+# import defusedxml.ElementTree as etree
 import lxml.etree as etree
 
 with open('./joined1886allbooks.xml') as fobj:
@@ -15,16 +15,22 @@ def title_verse_num():
     return elements
 
 def title_annots(elements):
-    title_annots = {}
-    for title_verse_num, element in elements: 
-        # elements.xpath('titleGroup/title/annotation'):
-        title_annot = element.xpath('titleGroup/title/annotation/notationQuote/para/span')
-        title_annot_cat = element.xpath('titleGroup/title/annotation/notationCategories/category')
-                
-        if len(title_annot) > 0 and len(title_annot_cat) > 0: title_annots[title_annot[0].text] = title_annot_cat[0].text
 
-    print(f"*{element.attrib['ID']}.0.{title_verse_num}*{'*'.join(title_annots)}")
+    for title_verse_num, element in elements: 
+        
+        title_annots = {}
+        for annot in element.xpath('titleGroup/title/annotation'):
+            title_annot = annot.xpath('notationQuote/para/span')
+            title_annot_cat = annot.xpath('notationCategories/category')
+
+            if len(title_annot) > 0 and \
+                    len(title_annot_cat) and \
+                    title_annot_cat[0].text != 'Misc' and \
+                    title_annot_cat[0].text != 'NoNote':
+                
+                title_annots[title_annot[0].text] = title_annot_cat[0].text
+
+        print(f"*{element.attrib['ID']}.0.{title_verse_num}*{','.join(title_annots.keys())}")
 
 elements = title_verse_num()
 title_annots(elements)
-# print(elements)
