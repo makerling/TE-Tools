08d62bc99070156f5812cdafbcf215585878cd2f
Stevan Vanderwerf <github@vanderling.com>
2021-10-27 22:45:18 -0500

    catching blank title_verse_num

--- Actions/QA/pythonconvert.py
+++ Actions/QA/pythonconvert.py
@@ -1,4 +1,3 @@
-# import defusedxml.ElementTree as etree
 import lxml.etree as etree
 
 with open('./joined1886allbooks.xml') as fobj:
@@ -15,7 +14,6 @@ def title_verse_num():
     return elements
 
 def title_annots(elements):
-
     for title_verse_num, element in elements: 
         
         title_annots = {}
@@ -30,6 +28,7 @@ def title_annots(elements):
                 
                 title_annots[title_annot[0].text] = title_annot_cat[0].text
 
+        title_verse_num = title_verse_num if len(title_verse_num) > 0 else 0
         print(f"*{element.attrib['ID']}.0.{title_verse_num}*{','.join(title_annots.keys())}")
 
 elements = title_verse_num()
