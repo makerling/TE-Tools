917380fb7174afdfa507801cd64a97504b188606
Stevan Vanderwerf <github@vanderling.com>
2021-10-29 19:26:18 -0500

    completed book title extraction

--- QA/pythonconvert.py
+++ QA/pythonconvert.py
@@ -10,26 +10,36 @@ def title_verse_num():
     for element in root.iter('book'):
         title_verse_num = "".join(element.xpath('titleGroup/title/annotation[1]/@status'))
         elements.append((title_verse_num,element))
-    print(elements)
     return elements
 
 def title_annots(elements):
+
+    titles_final = []
     for title_verse_num, element in elements: 
         
         title_annots = {}
-        for annot in element.xpath('titleGroup/title/annotation'):
-            title_annot = annot.xpath('notationQuote/para/span')
-            title_annot_cat = annot.xpath('notationCategories/category')
+        for annot in element.xpath('titleGroup/title'):
+            title_annot = annot.xpath('annotation/notationQuote/para/span')
+            # saving category of annotation (for future functionality)
+            title_annot_cat = annot.xpath('annotation/notationCategories/category')
 
+            # avoids null matches or matches that include secondary annotations (Misc, NoNote)
             if len(title_annot) > 0 and \
-                    len(title_annot_cat) and \
+                    len(title_annot_cat) > 0 and \
                     title_annot_cat[0].text != 'Misc' and \
                     title_annot_cat[0].text != 'NoNote':
                 
+                # appends the title annotation to the dictionary one level higher to catch all under each book
                 title_annots[title_annot[0].text] = title_annot_cat[0].text
+            title_tr = annot.xpath('trGroup/tr')
+            title_tr = title_tr[0].text if len(title_tr) > 0 else ''
 
-        title_verse_num = title_verse_num if len(title_verse_num) > 0 else 0
-        print(f"*{element.attrib['ID']}.0.{title_verse_num}*{','.join(title_annots.keys())}####")
+        title_verse_num = title_verse_num if len(title_verse_num) > 0 else 0 
+
+        title_final = f"*{element.attrib['ID']}.0.{title_verse_num}*{','.join(title_annots.keys())}####{title_tr}\p"
+        titles_final.append(title_final)
+    
+    return titles_final
 
 elements = title_verse_num()
-title_annots(elements)
+print(title_annots(elements))
